7177c1f11dbf627cd4d10eb4da9a48622cbdcda6
Murali Balusu <murali@halloapp.com>
2020-01-02 16:29:58 -0800

    Enable TLSpolicy for xmpp client-to-server connections
    - Enabled TLS policy to always use tls connection with the server.
    - Add some xmpp callbacks to log events during the secure connection of the xmpp stream.

--- Halloapp/Models/XMPPController.swift
+++ Halloapp/Models/XMPPController.swift
@@ -64,7 +64,7 @@ class XMPPController: NSObject, ObservableObject {
         self.xmppStream.hostPort = hostPort
         self.xmppStream.myJID = self.userJID
         
-//        self.xmppStream.startTLSPolicy = XMPPStreamStartTLSPolicy.allowed
+        self.xmppStream.startTLSPolicy = XMPPStreamStartTLSPolicy.allowed
         
 //        self.xmppStream.keepAliveInterval = 0.5;
         
@@ -235,24 +235,31 @@ extension XMPPController: XMPPStreamDelegate {
         return false
     }
     
-//    func xmppStreamWillConnect(_ sender: XMPPStream) {
-//        print("Stream: WillConnect")
-//    }
+    func xmppStreamWillConnect(_ sender: XMPPStream) {
+        print("Stream: WillConnect")
+    }
     
-//    func xmppStream(_ sender: XMPPStream, socketDidConnect socket: GCDAsyncSocket) {
-//        print("Stream: SocketDidConnect")
-//    }
+    func xmppStream(_ sender: XMPPStream, socketDidConnect socket: GCDAsyncSocket) {
+        print("Stream: SocketDidConnect")
+    }
     
-//    func xmppStreamDidStartNegotiation(_ sender: XMPPStream) {
-//        print("Stream: Start Negotiation")
-//    }
+    func xmppStreamDidStartNegotiation(_ sender: XMPPStream) {
+        print("Stream: Start Negotiation")
+    }
     
 /* ssl */
-//    func xmppStream(_ sender: XMPPStream, willSecureWithSettings settings: NSMutableDictionary) {
-//        print("Stream: willSecureWithSettings")
-//        settings.setObject(true, forKey:GCDAsyncSocketManuallyEvaluateTrust as NSCopying)
-//    }
-//
+    func xmppStream(_ sender: XMPPStream, willSecureWithSettings settings: NSMutableDictionary) {
+        print("Stream: willSecureWithSettings: \(settings)")
+        // The following key in settings has to be set only for self-signed certificates (or)
+        // those certificates that cannot be validated using the default settings as in SecTrustEvaluate.
+        // settings.setObject(true, forKey:GCDAsyncSocketManuallyEvaluateTrust as NSCopying)
+    }
+
+    func xmppStreamDidSecure(_ sender: XMPPStream) {
+        print("Stream: xmppStreamDidSecure.")
+    }
+
+// The following function has to be called only if the certificate has to be manually trusted in the xmppStream:willSecureWithSettings function.
 //    func xmppStream(_ sender: XMPPStream, didReceive trust: SecTrust, completionHandler: ((Bool) -> Void)) {
 //        print("Stream: didReceive trust")
 //        completionHandler(true)
