4a5eae733aa3f3c1b1e4635202d96130f921cdff
Igor Solomennikov <igor@halloapp.com>
2020-03-13 15:22:21 -0700

    Move String.sha1() to a file with other extensions of String.
    From ImageServer.swift.

--- Halloapp/Network/ImageServer.swift
+++ Halloapp/Network/ImageServer.swift
@@ -11,19 +11,6 @@ import Foundation
 import XMPPFramework
 import Alamofire
 
-import CommonCrypto
-
-extension String {
-    func sha1() -> String {
-        let data = Data(self.utf8)
-        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
-        }
-        let hexBytes = digest.map { String(format: "%02hhx", $0) }
-        return hexBytes.joined()
-    }
-}
 
 class ImageServer {
 
