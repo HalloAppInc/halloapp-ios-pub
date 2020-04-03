d5e58269bd2ff418861cff9f60c61cc643c753a3
Igor Solomennikov <igor@halloapp.com>
2020-04-03 14:16:32 -0700

    [verification] Improve handling of the server response and add more logging.

--- Halloapp/Models/Verification.swift
+++ Halloapp/Models/Verification.swift
@@ -45,6 +45,19 @@ final class Verification: ObservableObject {
             
         }
     }
+
+    func show(status: String, highlightInputField: Bool = false) {
+        self.status = status
+        if highlightInputField {
+            self.highlight = true
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.status = ""
+            if highlightInputField {
+                self.highlight = false
+            }
+        }
+    }
     
     func verify(userData: UserData) {
     
@@ -55,86 +68,56 @@ final class Verification: ObservableObject {
         let session = URLSession.shared
         let url = URL(string: "https://\(userData.hostName)/cgi-bin/register.sh?user=\(userData.phone)&code=\(self.code)")!
 
-        DDLogInfo("url: \(url)")
+        DDLogInfo("Validating code. url:[\(url)]")
         
-        let task = session.dataTask(with: url, completionHandler: { data, response, error in
-
-            
-            struct registerRes: Codable {
-                let user: String?
-                let pass: String?
-                let result: String?
-                
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                DDLogError("Error validating code. [\(error!)]")
+                DispatchQueue.main.async {
+                    self.show(status: "Error validating code. Please try again.")
+                }
+                return
             }
-            
-            if let data = data {
-                
-                DDLogInfo("verify data resonse: \(data)")
-                
-                do {
-                    let res = try JSONDecoder().decode(registerRes.self, from: data)
-                    
 
+            if let data = data {
+                DDLogInfo("Got code validation response: \(String(data: data, encoding: .utf8) ?? "<invalid>")")
+
+                do {
+                    let object = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    DDLogInfo("Decoded response [\(object)]")
                     DispatchQueue.main.async {
-                    
-                        if let password = res.pass {
-                            
+                        if let password = object["pass"] as? String {
+                            let userData = AppContext.shared.userData
                             userData.password = password
                             userData.logIn()
                             userData.save()
 
                             self.code = ""
                         } else {
-
-                            self.status = "Incorrect verification code"
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-                                self.status = ""
-                            }
-                            
+                            self.show(status: "Incorrect verification code")
+                            self.code = ""
                         }
-                        
-
                     }
-                    
-                } catch let error {
-                   DDLogError("\(error)")
+                } catch {
+                    DDLogError("Failed to parse response. [\(error)]")
                 }
-             }
-
-        })
+            }
+        }
         task.resume()
-        return
     }
     
     func validate() -> Bool {
-
         if (self.code == "") {
-            self.status = "Please enter a verification code"
-            self.highlight = true
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-                self.status = ""
-                self.highlight = false
-            }
+            self.show(status: "Please enter a verification code", highlightInputField: true)
             return false
         } else if (self.code.count < 6) {
-            self.status = "Please enter a valid verification code"
-            self.highlight = true
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-                self.status = ""
-                self.highlight = false
-            }
+            self.show(status: "Please enter a valid verification code", highlightInputField: true)
             return false
-        } else {
-            
-            return true
         }
+        return true
     }
-    
-    
-
 
     deinit {
         subCancellable.cancel()
     }
-    
 }
