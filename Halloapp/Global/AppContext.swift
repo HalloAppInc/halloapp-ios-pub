d9db8b2e5e2b010dd8caee387140903e99a1ee50
Igor Solomennikov <igor@halloapp.com>
2020-05-07 22:08:18 -0700

    Very basic crash reporting in logs.
    Only handle uncaught exceptions for now.

--- Halloapp/Global/AppContext.swift
+++ Halloapp/Global/AppContext.swift
@@ -12,59 +12,6 @@ import Foundation
 
 fileprivate var sharedContext: AppContext?
 
-class LogFormatter: NSObject, DDLogFormatter {
-
-    static private func logLevel(for logMessage: DDLogMessage) -> String {
-        switch logMessage.flag {
-        case .error: return "LL_E"
-        case .warning: return "LL_W"
-        case .info: return "LL_I"
-        case .debug: return "LL_D"
-        default: return "LL_V"
-        }
-    }
-
-    static func queueLabel(for logMessage: DDLogMessage) -> String {
-        let label = logMessage.queueLabel
-        var shortName = label.components(separatedBy: ".").last!
-        let maxLength = 14
-        if shortName.count > maxLength {
-            let suffixLength = 4
-            let prefix = shortName.prefix(maxLength - suffixLength - 1)
-            let suffix = shortName.suffix(suffixLength)
-            shortName = "\(prefix)…\(suffix)"
-        } else {
-            shortName = shortName.padding(toLength: maxLength, withPad: " ", startingAt: 0)
-        }
-        return shortName
-    }
-
-    func format(message logMessage: DDLogMessage) -> String? {
-        let queueName = LogFormatter.queueLabel(for: logMessage)
-        let logLevel = LogFormatter.logLevel(for: logMessage)
-        let logMessageStr = logMessage.message.replacingOccurrences(of: "\n", with: "\n\(logLevel)")
-
-        return "\(queueName) \(logLevel) \(logMessageStr)"
-    }
-}
-
-class FileLogFormatter: LogFormatter {
-    private let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-        dateFormatter.locale = Locale(identifier: "en_US")
-        return dateFormatter
-    }()
-
-    override func format(message logMessage: DDLogMessage) -> String? {
-        if let logMessageStr = super.format(message: logMessage) {
-            let dateStr = dateFormatter.string(from: logMessage.timestamp)
-            return "\(dateStr) \(logMessage.threadID) \(logMessageStr)"
-        }
-        return nil
-    }
-}
-
 struct AppContext {
     // MARK: - Constants
     static let appGroupName = "group.com.halloapp.shared"
@@ -138,6 +85,9 @@ struct AppContext {
         let osLogger = DDOSLogger.sharedInstance
         osLogger.logFormatter = LogFormatter()
         DDLog.add(osLogger)
+
+        // Register crash handler(s)
+        CrashHandler.registerHandlers()
 
         // This is needed to encode/decode protobuf in FeedPostInfo.
         ValueTransformer.setValueTransformer(FeedPostReceiptInfoTransformer(), forName: .feedPostReceiptInfoTransformer)
---
+++ Halloapp/Global/Logging/LogFormatter.swift
@@ -0,0 +1,62 @@
+//
+//  LogFormatter.swift
+//  HalloApp
+//
+//  Created by Igor Solomennikov on 5/7/20.
+//  Copyright © 2020 Halloapp, Inc. All rights reserved.
+//
+
+import CocoaLumberjack
+
+class LogFormatter: NSObject, DDLogFormatter {
+
+    static private func logLevel(for logMessage: DDLogMessage) -> String {
+        switch logMessage.flag {
+        case .error: return "LL_E"
+        case .warning: return "LL_W"
+        case .info: return "LL_I"
+        case .debug: return "LL_D"
+        default: return "LL_V"
+        }
+    }
+
+    static func queueLabel(for logMessage: DDLogMessage) -> String {
+        let label = logMessage.queueLabel
+        var shortName = label.components(separatedBy: ".").last!
+        let maxLength = 14
+        if shortName.count > maxLength {
+            let suffixLength = 4
+            let prefix = shortName.prefix(maxLength - suffixLength - 1)
+            let suffix = shortName.suffix(suffixLength)
+            shortName = "\(prefix)…\(suffix)"
+        } else {
+            shortName = shortName.padding(toLength: maxLength, withPad: " ", startingAt: 0)
+        }
+        return shortName
+    }
+
+    func format(message logMessage: DDLogMessage) -> String? {
+        let queueName = LogFormatter.queueLabel(for: logMessage)
+        let logLevel = LogFormatter.logLevel(for: logMessage)
+        let logMessageStr = logMessage.message.replacingOccurrences(of: "\n", with: "\n\(logLevel) ")
+
+        return "\(queueName) \(logLevel) \(logMessageStr)"
+    }
+}
+
+class FileLogFormatter: LogFormatter {
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        dateFormatter.locale = Locale(identifier: "en_US")
+        return dateFormatter
+    }()
+
+    override func format(message logMessage: DDLogMessage) -> String? {
+        if let logMessageStr = super.format(message: logMessage) {
+            let dateStr = dateFormatter.string(from: logMessage.timestamp)
+            return "\(dateStr) \(logMessage.threadID) \(logMessageStr)"
+        }
+        return nil
+    }
+}
