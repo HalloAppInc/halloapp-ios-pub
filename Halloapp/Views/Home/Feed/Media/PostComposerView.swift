d40068630a29ec363696ac4b5a1567ca8ae0ba08
Igor Solomennikov <igor@halloapp.com>
2020-06-04 23:17:52 -0700

    Prettify post composer a little bit.
    Make text field visible when in Dark Mode.

--- Halloapp/Views/Home/Feed/Media/PostComposerView.swift
+++ Halloapp/Views/Home/Feed/Media/PostComposerView.swift
@@ -3,38 +3,16 @@ import CocoaLumberjack
 import SwiftUI
 import UIKit
 
-struct DismissingKeyboard: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .onTapGesture {
-                let keyWindow = UIApplication.shared.connectedScenes
-                    .filter({$0.activationState == .foregroundActive})
-                    .map({$0 as? UIWindowScene})
-                    .compactMap({$0})
-                    .first?.windows
-                    .filter({$0.isKeyWindow}).first
-                keyWindow?.endEditing(true)
-        }
-    }
-}
-
-
 struct PostComposerView: View {
-    private let userData = MainAppContext.shared.userData
+
     private let imageServer = ImageServer()
-
-    var mediaItemsToPost: [PendingMedia]
-
-    var didFinish: () -> Void
+    private let mediaItemsToPost: [PendingMedia]
+    private let didFinish: () -> Void
 
     @State var isJustText: Bool = false
-    
     @State var msgToSend = ""
-
     @State var isShareClicked: Bool = false
-    
     @State var isReadyToPost: Bool = false
-
     @State var numberOfFailedUploads: Int = 0
 
     init(mediaItemsToPost: [PendingMedia], didFinish: @escaping () -> Void) {
@@ -43,92 +21,81 @@ struct PostComposerView: View {
     }
     
     var body: some View {
-        return VStack {
+        return NavigationView {
+            VStack {
+                Spacer()
+                    .frame(height: 16)
 
-            Divider()
-                .frame(height: UIScreen.main.bounds.height < 812 ? 10 : 40)
-                .hidden()
-                .modifier(DismissingKeyboard())
+                if self.mediaItemsToPost.count > 0 {
+                    MediaPreviewSlider(self.mediaItemsToPost.map { FeedMedia($0, feedPostId: "") })
+                        .frame(height: 200, alignment: .center)
 
-            HStack() {
+                    if self.numberOfFailedUploads > 1 {
+                        Text("Failed to upload \(self.numberOfFailedUploads) media items. Please try again.")
+                            .foregroundColor(.red)
+                    } else if self.numberOfFailedUploads > 0 {
+                        Text("Failed to upload media. Please try again.")
+                            .foregroundColor(.red)
+                    }
+
+                    Spacer()
+                        .frame(height: 16)
+                }
+
+                HStack {
+                    TextView(text: self.$msgToSend)
+                        .cornerRadius(10)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: isJustText ? 100 : 70)
+                }
+                .padding(.horizontal)
+
+                Spacer()
+                    .frame(height: 16)
 
                 Button(action: {
-                    self.imageServer.cancel()
-                    self.didFinish()
+                    if self.isShareClicked {
+                        return
+                    }
+
+                    if self.isJustText && self.msgToSend == "" {
+                        return
+                    }
+
+                    if self.numberOfFailedUploads > 0 {
+                        return
+                    }
+
+                    if self.isReadyToPost {
+                        self.isShareClicked = true
+                        MainAppContext.shared.feedData.post(text: self.msgToSend.trimmingCharacters(in: .whitespacesAndNewlines), media: self.mediaItemsToPost)
+                        self.didFinish()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            self.msgToSend = ""
+                            self.isShareClicked = false
+                        }
+                    }
                 }) {
-                    Text("Cancel")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(Color.gray)
-                        .padding(EdgeInsets(top: 15, leading: 20, bottom: 0, trailing: 15))
+                    Text("SHARE")
+                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                        .background((self.isJustText && self.msgToSend != "") || (!self.isJustText && self.isReadyToPost && self.numberOfFailedUploads == 0) ? Color.blue : Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .shadow(radius: 2)
                 }
+
                 Spacer()
             }
-            .modifier(DismissingKeyboard())
-
-            Divider()
-                .frame(height: isJustText ? 15 : 0)
-                .hidden()
-
-            if self.mediaItemsToPost.count > 0 {
-                MediaPreviewSlider(self.mediaItemsToPost.map { FeedMedia($0, feedPostId: "") })
-                    .frame(height: 200, alignment: .center)
-
-                if self.numberOfFailedUploads > 1 {
-                    Text("Failed to upload \(self.numberOfFailedUploads) media items. Please try again.")
-                        .foregroundColor(.red)
-                } else if self.numberOfFailedUploads > 0 {
-                    Text("Failed to upload media. Please try again.")
-                        .foregroundColor(.red)
-                }
-            }
-        
-            HStack {
-                TextView(text: self.$msgToSend)
-                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: isJustText ? 100 : 70)
-                    .cornerRadius(10)
-            }
-            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-            .modifier(DismissingKeyboard())
-
-            Divider()
-                .frame(height: isJustText ? 20 : 5)
-                .hidden()
-                .modifier(DismissingKeyboard())
-
-            Button(action: {
-                if self.isShareClicked {
-                    return
-                }
-
-                if self.isJustText && self.msgToSend == "" {
-                    return
-                }
-
-                if self.numberOfFailedUploads > 0 {
-                    return
-                }
-
-                if self.isReadyToPost {
-                    self.isShareClicked = true
-                    MainAppContext.shared.feedData.post(text: self.msgToSend.trimmingCharacters(in: .whitespacesAndNewlines), media: self.mediaItemsToPost)
-                    self.didFinish()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                        self.msgToSend = ""
-                        self.isShareClicked = false
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarItems(leading:
+                HStack {
+                    Button(action: {
+                        self.imageServer.cancel()
+                        self.didFinish()
+                    }) {
+                        Text("Cancel")
                     }
-                }
-            }) {
-                Text("SHARE")
-                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-                    .background((self.isJustText && self.msgToSend != "") || (!self.isJustText && self.isReadyToPost && self.numberOfFailedUploads == 0) ? Color.blue : Color.gray)
-                    .foregroundColor(.white)
-                    .cornerRadius(20)
-                    .shadow(radius: 2)
-            }
-
-            Spacer()
-                .modifier(DismissingKeyboard())
+            })
+            .background(Color(UIColor.feedBackgroundColor))
         }
         .onAppear {
             if self.mediaItemsToPost.isEmpty {
@@ -138,7 +105,48 @@ struct PostComposerView: View {
                 self.imageServer.upload(self.mediaItemsToPost, isReady: self.$isReadyToPost, numberOfFailedUploads: self.$numberOfFailedUploads)
             }
         }
-        .background(Color(UIColor.systemBackground))
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+fileprivate struct TextView: UIViewRepresentable {
+    @Binding var text: String
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+        let myTextView = UITextView()
+        myTextView.delegate = context.coordinator
+        myTextView.font = .preferredFont(forTextStyle: .body)
+        myTextView.isScrollEnabled = true
+        myTextView.isEditable = true
+        myTextView.isUserInteractionEnabled = true
+        myTextView.backgroundColor = UIColor.textFieldBackgroundColor
+        myTextView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return myTextView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+
+    class Coordinator : NSObject, UITextViewDelegate {
+
+        var parent: TextView
+
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return true
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
     }
 }
 
