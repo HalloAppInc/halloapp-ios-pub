c18e7233106c30f6e69570f29c0dcff2a072277c
Igor Solomennikov <igor@halloapp.com>
2020-06-26 17:13:59 -0700

    Add Color.lavaOrange - UIColor counterpart to be used in SwiftUI.

---
+++ Halloapp/Components/Extensions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  HalloApp
+//
+//  Created by Igor Solomennikov on 6/26/20.
+//  Copyright © 2020 Halloapp, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static var lavaOrange: Color {
+        get { Color("LavaOrange") }
+    }
+}
