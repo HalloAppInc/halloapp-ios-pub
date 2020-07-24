348c1f6c45d6c780bea846b7fb7c0b2b7bfa98b6
Igor Solomennikov <igor@halloapp.com>
2020-07-24 13:15:43 -0700

    Show connection status at the top of the screen in debug builds.

--- Halloapp/Landing.swift
+++ Halloapp/Landing.swift
@@ -8,14 +8,34 @@
 
 import SwiftUI
 
+fileprivate struct DebugInfoView: View {
+    @ObservedObject var xmppController = MainAppContext.shared.xmppController
+
+    var body: some View {
+        VStack {
+            Text("Connection Status: \(String(describing:xmppController.connectionState))")
+                .font(.footnote)
+        }
+        .padding(.vertical, 4)
+        .frame(maxWidth: .infinity)
+        .background(Color.gray.opacity(0.5))
+    }
+}
+
 struct Landing: View {
     @ObservedObject var userData = MainAppContext.shared.userData
 
     var body: some View {
         ZStack {
             if (self.userData.isLoggedIn) {
+                #if DEBUG
                 HomeView()
                     .edgesIgnoringSafeArea(.all)
+                    .overlay(DebugInfoView().allowsHitTesting(false), alignment: .top)
+                #else
+                HomeView()
+                    .edgesIgnoringSafeArea(.all)
+                #endif
             } else {
                 VerificationView()
                     .edgesIgnoringSafeArea(.all)
