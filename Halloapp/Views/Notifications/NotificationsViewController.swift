2c19cacffb077b3fbed1190e2cee2bf104c9a3f1
Igor Solomennikov <igor@halloapp.com>
2020-08-31 21:16:22 -0700

    Improve support for larger font sizes in Activity Center.

--- Halloapp/Views/Notifications/NotificationsViewController.swift
+++ Halloapp/Views/Notifications/NotificationsViewController.swift
@@ -6,30 +6,30 @@
 //  Copyright © 2019 Halloapp, Inc. All rights reserved.
 //
 
-import Combine
 import Core
 import CoreData
 import UIKit
 
 class NotificationsViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+
     private static let cellReuseIdentifier = "NotificationsTableViewCell"
 
-    private var dataSource: UITableViewDiffableDataSourceReference?
-    private var fetchedResultsController: NSFetchedResultsController<FeedNotification>?
+    private var dataSource: UITableViewDiffableDataSourceReference!
+    private var fetchedResultsController: NSFetchedResultsController<FeedNotification>!
 
     // MARK: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.navigationItem.title = "Activity"
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "NavbarClose"), style: .plain, target: self, action: #selector(cancelAction))
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Read All", style: .plain, target: self, action: #selector(markAllNotificationsRead))
+        navigationItem.title = "Activity"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "NavbarClose"), style: .plain, target: self, action: #selector(cancelAction))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Read All", style: .plain, target: self, action: #selector(markAllNotificationsRead))
 
-        self.tableView.register(NotificationTableViewCell.self, forCellReuseIdentifier: NotificationsViewController.cellReuseIdentifier)
-        self.tableView.separatorStyle = .none
+        tableView.register(NotificationTableViewCell.self, forCellReuseIdentifier: NotificationsViewController.cellReuseIdentifier)
+        tableView.separatorStyle = .none
 
-        self.dataSource = UITableViewDiffableDataSourceReference(tableView: self.tableView) { tableView, indexPath, objectID in
+        dataSource = UITableViewDiffableDataSourceReference(tableView: tableView) { tableView, indexPath, objectID in
             let cell = tableView.dequeueReusableCell(withIdentifier: NotificationsViewController.cellReuseIdentifier, for: indexPath) as! NotificationTableViewCell
             if let notification = try? MainAppContext.shared.feedData.viewContext.existingObject(with: objectID as! NSManagedObjectID) as? FeedNotification {
                 cell.configure(with: notification)
@@ -39,29 +39,27 @@ class NotificationsViewController: UITableViewController, NSFetchedResultsContro
 
         let fetchRequest: NSFetchRequest<FeedNotification> = FeedNotification.fetchRequest()
         fetchRequest.sortDescriptors = [ NSSortDescriptor(keyPath: \FeedNotification.timestamp, ascending: false) ]
-        self.fetchedResultsController =
+        fetchedResultsController =
             NSFetchedResultsController<FeedNotification>(fetchRequest: fetchRequest, managedObjectContext: MainAppContext.shared.feedData.viewContext,
                                                          sectionNameKeyPath: nil, cacheName: nil)
-        self.fetchedResultsController?.delegate = self
+        fetchedResultsController.delegate = self
         do {
-            try self.fetchedResultsController!.performFetch()
-        } catch {
-            return
-        }
+            try fetchedResultsController!.performFetch()
+        } catch { }
     }
 
     // MARK: Fetched Results Controller
 
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
         var reloadData = false
-        if let currentObjectIDs = self.dataSource?.snapshot().itemIdentifiers as? [NSManagedObjectID],
+        if let currentObjectIDs = dataSource.snapshot().itemIdentifiers as? [NSManagedObjectID],
             let updatedObjectIDs = snapshot.itemIdentifiers as? [NSManagedObjectID] {
             // If this method is called, but list of object IDs is the same it means
             // that one or more FeedNotification objects have been changed.
             // To reflect changes we call UITableView.reloadData.
             reloadData = currentObjectIDs == updatedObjectIDs
         }
-        self.dataSource?.applySnapshot(snapshot, animatingDifferences: self.view.window != nil) {
+        dataSource.applySnapshot(snapshot, animatingDifferences: view.window != nil) {
             if reloadData {
                 self.tableView.reloadData()
             }
@@ -71,20 +69,20 @@ class NotificationsViewController: UITableViewController, NSFetchedResultsContro
     // MARK: Table View
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let notification = self.fetchedResultsController?.object(at: indexPath),
-              let feedPost = MainAppContext.shared.feedData.feedPost(with: notification.postId) else {
+        let notification = fetchedResultsController.object(at: indexPath)
+        guard let feedPost = MainAppContext.shared.feedData.feedPost(with: notification.postId) else {
             tableView.deselectRow(at: indexPath, animated: true)
             return
         }
         let commentsViewController = CommentsViewController(feedPostId: feedPost.id)
         commentsViewController.highlightedCommentId = notification.commentId
-        self.navigationController?.pushViewController(commentsViewController, animated: true)
+        navigationController?.pushViewController(commentsViewController, animated: true)
     }
 
     // MARK: UI Actions
 
     @objc private func cancelAction() {
-        self.dismiss(animated: true)
+        dismiss(animated: true)
     }
 
     @objc private func markAllNotificationsRead() {
@@ -93,25 +91,25 @@ class NotificationsViewController: UITableViewController, NSFetchedResultsContro
             MainAppContext.shared.feedData.markNotificationsAsRead()
         })
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        self.present(actionSheet, animated: true)
+        present(actionSheet, animated: true)
     }
 }
 
 fileprivate class NotificationTableViewCell: UITableViewCell {
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.setupView()
+        setupView()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.setupView()
+        setupView()
     }
 
     private lazy var contactImage: AvatarView = {
         let imageView = AvatarView()
-        imageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
@@ -127,8 +125,6 @@ fileprivate class NotificationTableViewCell: UITableViewCell {
         let badge = CircleView()
         badge.fillColor = .systemGreen
         badge.translatesAutoresizingMaskIntoConstraints = false
-        badge.heightAnchor.constraint(equalToConstant: 6).isActive = true
-        badge.widthAnchor.constraint(equalToConstant: 6).isActive = true
         return badge
     }()
 
@@ -137,33 +133,46 @@ fileprivate class NotificationTableViewCell: UITableViewCell {
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 8
-        imageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
         return imageView
     }()
 
     private func setupView() {
-        let hStack = UIStackView(arrangedSubviews: [ self.contactImage, self.notificationTextLabel, self.mediaPreview ])
-        hStack.axis = .horizontal
-        hStack.spacing = 8
-        hStack.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addSubview(hStack)
-        hStack.leadingAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.leadingAnchor).isActive = true
-        hStack.topAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.topAnchor).isActive = true
-        hStack.trailingAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.trailingAnchor).isActive = true
-        hStack.bottomAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.bottomAnchor).isActive = true
+        contentView.addSubview(unreadBadge)
+        contentView.addSubview(contactImage)
+        contentView.addSubview(notificationTextLabel)
+        contentView.addSubview(mediaPreview)
 
-        self.contentView.addSubview(self.unreadBadge)
-        self.unreadBadge.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
-        self.unreadBadge.trailingAnchor.constraint(equalTo: hStack.leadingAnchor, constant: -4).isActive = true
+        contentView.addConstraints([
+            unreadBadge.heightAnchor.constraint(equalToConstant: 6),
+            unreadBadge.widthAnchor.constraint(equalTo: unreadBadge.heightAnchor),
+            unreadBadge.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            unreadBadge.trailingAnchor.constraint(equalTo: contactImage.leadingAnchor, constant: -4),
+
+            contactImage.heightAnchor.constraint(equalToConstant: 44),
+            contactImage.heightAnchor.constraint(equalTo: contactImage.widthAnchor),
+            contactImage.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            contactImage.topAnchor.constraint(greaterThanOrEqualTo: contentView.layoutMarginsGuide.topAnchor),
+            contactImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+
+            notificationTextLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: contactImage.trailingAnchor, multiplier: 1),
+            notificationTextLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            notificationTextLabel.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+
+            mediaPreview.heightAnchor.constraint(equalToConstant: 44),
+            mediaPreview.heightAnchor.constraint(equalTo: mediaPreview.widthAnchor),
+            mediaPreview.leadingAnchor.constraint(equalToSystemSpacingAfter: notificationTextLabel.trailingAnchor, multiplier: 1),
+            mediaPreview.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            mediaPreview.topAnchor.constraint(greaterThanOrEqualTo: contentView.layoutMarginsGuide.topAnchor),
+            mediaPreview.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
     }
 
     func configure(with notification: FeedNotification) {
-        self.unreadBadge.isHidden = notification.read
-        self.backgroundColor = notification.read ? .systemBackground : .systemGray5
-        self.notificationTextLabel.attributedText = notification.formattedText
-        self.mediaPreview.image = notification.image
-        
+        backgroundColor = notification.read ? .systemBackground : .systemGray5
+
+        unreadBadge.isHidden = notification.read
+        notificationTextLabel.attributedText = notification.formattedText
+        mediaPreview.image = notification.image
         contactImage.configure(with: notification.userId, using: MainAppContext.shared.avatarStore)
     }
     
