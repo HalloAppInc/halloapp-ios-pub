5e6e20ae7707b2582182a204a3a42589b9a2e3f5
Igor Solomennikov <igor@halloapp.com>
2020-12-02 16:13:14 -0800

    Remove back arrow labels #381

--- Halloapp/Components/Extensions/UINavigationBar.swift
+++ Halloapp/Components/Extensions/UINavigationBar.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+extension UIBarButtonItemAppearance {
+
+    class var transparentAppearance: UIBarButtonItemAppearance {
+        let appearance = UIBarButtonItemAppearance(style: .plain)
+        appearance.normal.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.clear ]
+        appearance.highlighted.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.clear ]
+        appearance.disabled.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.clear ]
+        appearance.focused.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.clear ]
+        return appearance
+    }
+}
+
 extension UINavigationBarAppearance {
 
-    func configureTitleTextAttributes() {
+    private func configureTitleTextAttributes() {
         titleTextAttributes = [
             .font: UIFont.gothamFont(ofFixedSize: 15, weight: .medium),
             .foregroundColor: UIColor.label.withAlphaComponent(0.9)
@@ -25,6 +37,7 @@ extension UINavigationBarAppearance {
             //TODO: proper mask image.
             appearance.setBackIndicatorImage(UIImage(named: "NavbarBack"), transitionMaskImage: UIImage(named: "NavbarBack"))
             appearance.backgroundColor = .feedBackground
+            appearance.backButtonAppearance = .transparentAppearance
             appearance.shadowColor = nil
             return appearance
         }
@@ -38,6 +51,7 @@ extension UINavigationBarAppearance {
              //TODO: proper mask image.
             appearance.setBackIndicatorImage(UIImage(named: "NavbarBack"), transitionMaskImage: UIImage(named: "NavbarBack"))
             appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+            appearance.backButtonAppearance = .transparentAppearance
             appearance.shadowColor = nil
             return appearance
         }
@@ -50,6 +64,7 @@ extension UINavigationBarAppearance {
             appearance.configureTitleTextAttributes()
              //TODO: proper mask image.
             appearance.setBackIndicatorImage(UIImage(named: "NavbarBack"), transitionMaskImage: UIImage(named: "NavbarBack"))
+            appearance.backButtonAppearance = .transparentAppearance
             return appearance
         }
     }
