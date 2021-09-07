8afee9d7bb98ee1011de2ba34b9ffa259aef0132
Stefan Fidanov <stefan@halloapp.com>
2021-09-07 17:43:19 +0300

    Updates audio record control to latest design.

--- Halloapp/Views/Home/AudioRecorderControlView.swift
+++ Halloapp/Views/Home/AudioRecorderControlView.swift
@@ -17,6 +17,8 @@ protocol AudioRecorderControlViewDelegate: AnyObject {
 class AudioRecorderControlView: UIView {
     public weak var delegate: AudioRecorderControlViewDelegate?
 
+    private var lockButtonAnimator: UIViewPropertyAnimator?
+
     private lazy var mainButton: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Microphone")?.withTintColor(.primaryBlue))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -42,39 +44,60 @@ class AudioRecorderControlView: UIView {
         icon.translatesAutoresizingMaskIntoConstraints = false
         icon.contentMode = .scaleAspectFit
 
-        let button = UIImageView(image: UIImage(named: "AudioDropTop"))
+        let button = UIView()
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .primaryBlue
+        button.layer.cornerRadius = 19
+        button.layer.masksToBounds = true
 
         button.widthAnchor.constraint(equalToConstant: 38).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 51).isActive = true
-
-        button.addSubview(icon)
-        icon.widthAnchor.constraint(equalToConstant: 24).isActive = true
-        icon.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        icon.topAnchor.constraint(equalTo: button.topAnchor, constant: 7).isActive = true
-        icon.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
-
-        return button
-    } ()
-
-    private lazy var cancelButton: UIView = {
-        let icon = UIImageView(image: UIImage(named: "NavbarTrashBinWithLid")?.withTintColor(.white))
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        icon.contentMode = .scaleAspectFit
-
-        let button = UIImageView(image: UIImage(named: "AudioDropLeft"))
-        button.translatesAutoresizingMaskIntoConstraints = false
-
-        button.widthAnchor.constraint(equalToConstant: 51).isActive = true
         button.heightAnchor.constraint(equalToConstant: 38).isActive = true
 
         button.addSubview(icon)
         icon.widthAnchor.constraint(equalToConstant: 24).isActive = true
         icon.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        icon.leftAnchor.constraint(equalTo: button.leftAnchor, constant: 7).isActive = true
+        icon.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
         icon.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
 
         return button
+    } ()
+
+    private lazy var cancelButton: UIView = {
+        let button = UILabel()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.font = .systemFont(ofSize: 15, weight: .light)
+        button.textColor = .black.withAlphaComponent(0.5)
+        button.textAlignment = .center
+        button.text = "slide to cancel"
+
+        button.widthAnchor.constraint(equalToConstant: 98).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 16).isActive = true
+
+        return button
+    } ()
+
+    private lazy var leftArrow: UIView = {
+        let config = UIImage.SymbolConfiguration(weight: .bold)
+        let arrow = UIImageView(image: UIImage(systemName: "chevron.left", withConfiguration: config)?.withTintColor(.primaryBlue))
+        arrow.translatesAutoresizingMaskIntoConstraints = false
+        arrow.contentMode = .scaleAspectFit
+
+        arrow.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        arrow.heightAnchor.constraint(equalToConstant: 21).isActive = true
+
+        return arrow
+    } ()
+
+    private lazy var topArrow: UIView = {
+        let config = UIImage.SymbolConfiguration(weight: .bold)
+        let arrow = UIImageView(image: UIImage(systemName: "chevron.up", withConfiguration: config)?.withTintColor(.primaryBlue))
+        arrow.translatesAutoresizingMaskIntoConstraints = false
+        arrow.contentMode = .scaleAspectFit
+
+        arrow.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        arrow.heightAnchor.constraint(equalToConstant: 21).isActive = true
+
+        return arrow
     } ()
 
     private lazy var expandingContainer: UIView = {
@@ -95,6 +118,14 @@ class AudioRecorderControlView: UIView {
         container.addSubview(cancelButton)
         cancelButton.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
 
+        container.addSubview(leftArrow)
+        leftArrow.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        leftArrow.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 7).isActive = true
+
+        container.addSubview(topArrow)
+        topArrow.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        topArrow.topAnchor.constraint(equalTo: container.topAnchor, constant: 7).isActive = true
+
         return container
     }()
 
@@ -104,11 +135,11 @@ class AudioRecorderControlView: UIView {
     private lazy var expandingContainerHeight: NSLayoutConstraint = {
         return expandingContainer.heightAnchor.constraint(equalToConstant: expandMinSize)
     }()
-    private lazy var lockButtonTop: NSLayoutConstraint = {
-        return lockButton.topAnchor.constraint(equalTo: expandingContainer.topAnchor, constant: actionOffset)
+    private lazy var lockButtonVertical: NSLayoutConstraint = {
+        return lockButton.bottomAnchor.constraint(equalTo: expandingContainer.topAnchor, constant: actionOffset)
     }()
-    private lazy var cancelButtonLeft: NSLayoutConstraint = {
-        return cancelButton.leftAnchor.constraint(equalTo: expandingContainer.leftAnchor, constant: actionOffset)
+    private lazy var cancelButtonHorizontal: NSLayoutConstraint = {
+        return cancelButton.rightAnchor.constraint(equalTo: expandingContainer.leftAnchor, constant: actionOffset)
     }()
 
     private var isActive = false
@@ -118,7 +149,7 @@ class AudioRecorderControlView: UIView {
 
     private let expandMinSize: CGFloat = 40
     private let expandMaxSize: CGFloat = 230
-    private let actionOffset: CGFloat = 12
+    private let actionOffset: CGFloat = -8
     private let actionInProgressThreshold: CGFloat = 20
     private let actionActivationThreshold: CGFloat = 115
     private let actionDoneThreshold: CGFloat = 135
@@ -141,8 +172,8 @@ class AudioRecorderControlView: UIView {
         expandingContainerWidth.isActive = true
         expandingContainerHeight.isActive = true
 
-        lockButtonTop.isActive = true
-        cancelButtonLeft.isActive = true
+        lockButtonVertical.isActive = true
+        cancelButtonHorizontal.isActive = true
     }
 
     required init?(coder: NSCoder) {
@@ -182,26 +213,38 @@ class AudioRecorderControlView: UIView {
             } else if distanceY > actionActivationThreshold {
                 let progress = (distanceY - actionActivationThreshold) / (actionDoneThreshold - actionActivationThreshold)
                 cancelButton.alpha = 0
+                leftArrow.alpha = 0
+                topArrow.alpha = 0
                 expandingContainerWidth.constant = expandMinSize
                 expandingContainerHeight.constant = expandMinSize
                 lockButton.alpha = 1 - progress
-                lockButtonTop.constant = 12 - (expandMaxSize - expandMinSize) / 2 - diff
+
+                stopLockButtonLevitation()
+                lockButtonVertical.constant = actionOffset - (expandMaxSize - expandMinSize) / 2 - diff
             } else if distanceY > actionInProgressThreshold {
                 let progress = diff / (actionActivationThreshold - actionInProgressThreshold)
                 cancelButton.alpha = 1 - progress
+                leftArrow.alpha = 1 - progress
+                topArrow.alpha = 1 - progress
                 expandingContainerWidth.constant = 190 * (1 - progress) + 40
                 expandingContainerHeight.constant = 190 * (1 - progress) + 40
                 expandingContainer.layer.cornerRadius = expandingContainerWidth.constant / 2
                 blurredBackground.layer.cornerRadius = expandingContainerWidth.constant / 2
                 lockButton.alpha = 1
-                lockButtonTop.constant = 12 - (expandMaxSize - expandingContainerWidth.constant) / 2 - diff
+
+                stopLockButtonLevitation()
+                lockButtonVertical.constant = actionOffset - (expandMaxSize - expandingContainerWidth.constant) / 2 - diff
             } else {
                 isLockInProgress = false
                 cancelButton.alpha = 1
+                leftArrow.alpha = 1
+                topArrow.alpha = 1
                 expandingContainerWidth.constant = expandMaxSize
                 expandingContainerHeight.constant = expandMaxSize
                 lockButton.alpha = 1
-                lockButtonTop.constant = actionOffset
+                lockButtonVertical.constant = actionOffset
+
+                levitateLockButton()
             }
         } else if isCancelInProgress {
             let diff = (distanceX - actionInProgressThreshold)
@@ -209,33 +252,61 @@ class AudioRecorderControlView: UIView {
             if distanceX > actionDoneThreshold {
                 delegate?.audioRecorderControlViewFinished(self, cancel: true)
                 hide()
-            } else if distanceY > actionActivationThreshold {
+            } else if distanceX > actionActivationThreshold {
                 let progress = (distanceX - actionActivationThreshold) / (actionDoneThreshold - actionActivationThreshold)
                 lockButton.alpha = 0
+                leftArrow.alpha = 0
+                topArrow.alpha = 0
                 expandingContainerWidth.constant = expandMinSize
                 expandingContainerHeight.constant = expandMinSize
                 cancelButton.alpha = 1 - progress
-                cancelButtonLeft.constant = 12 - (expandMaxSize - expandMinSize) / 2 - diff
+                cancelButtonHorizontal.constant = actionOffset - (expandMaxSize - expandMinSize) / 2 - diff
+
+                stopLockButtonLevitation()
             } else if distanceX > actionInProgressThreshold {
                 let progress = diff / (actionActivationThreshold - actionInProgressThreshold)
                 lockButton.alpha = 1 - progress
+                leftArrow.alpha = 1 - progress
+                topArrow.alpha = 1 - progress
                 expandingContainerWidth.constant = 190 * (1 - progress) + 40
                 expandingContainerHeight.constant = 190 * (1 - progress) + 40
                 expandingContainer.layer.cornerRadius = expandingContainerWidth.constant / 2
                 blurredBackground.layer.cornerRadius = expandingContainerWidth.constant / 2
                 cancelButton.alpha = 1
-                cancelButtonLeft.constant = 12 - (expandMaxSize - expandingContainerWidth.constant) / 2 - diff
+                cancelButtonHorizontal.constant = actionOffset - (expandMaxSize - expandingContainerWidth.constant) / 2 - diff
+
+                stopLockButtonLevitation()
             } else {
                 isCancelInProgress = false
                 lockButton.alpha = 1
+                leftArrow.alpha = 1
+                topArrow.alpha = 1
                 expandingContainerWidth.constant = expandMaxSize
                 expandingContainerHeight.constant = expandMaxSize
                 cancelButton.alpha = 1
-                cancelButtonLeft.constant = actionOffset
+                cancelButtonHorizontal.constant = actionOffset
+
+                levitateLockButton()
             }
         }
 
         layoutIfNeeded()
+    }
+
+    private func levitateLockButton(_ reversed: Bool = false) {
+        let target = reversed ? lockButtonVertical.constant + 20 : lockButtonVertical.constant - 20
+
+        lockButtonAnimator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.6, delay: 0, options: [.curveEaseInOut], animations: { [weak self] in
+            self?.lockButtonVertical.constant = target
+            self?.layoutIfNeeded()
+        }, completion: { [weak self] _ in
+            self?.levitateLockButton(!reversed)
+        })
+    }
+
+    private func stopLockButtonLevitation() {
+        lockButtonAnimator?.stopAnimation(true)
+        lockButtonAnimator = nil
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -258,12 +329,14 @@ class AudioRecorderControlView: UIView {
         blurredBackground.layer.cornerRadius = expandingContainerWidth.constant / 2
         layoutIfNeeded()
 
-        UIView.animate(withDuration: 0.3) {
+        UIView.animate(withDuration: 0.3, animations: {
             self.expandingContainerWidth.constant = self.expandMaxSize
             self.expandingContainerHeight.constant = self.expandMaxSize
             self.expandingContainer.layer.cornerRadius = self.expandingContainerWidth.constant / 2
             self.blurredBackground.layer.cornerRadius = self.expandingContainerWidth.constant / 2
             self.layoutIfNeeded()
+        }) { _ in
+            self.levitateLockButton()
         }
     }
 
@@ -274,8 +347,13 @@ class AudioRecorderControlView: UIView {
         isCancelInProgress = false
         cancelButton.alpha = 1
         lockButton.alpha = 1
-        lockButtonTop.constant = actionOffset
-        cancelButtonLeft.constant = actionOffset
+        leftArrow.alpha = 1
+        topArrow.alpha = 1
+
+        stopLockButtonLevitation()
+
+        lockButtonVertical.constant = actionOffset
+        cancelButtonHorizontal.constant = actionOffset
         layoutIfNeeded()
     }
 }
