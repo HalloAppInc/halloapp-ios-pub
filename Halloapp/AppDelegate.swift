7114253f92bfcd7f53f58a42471ffb7c28f18493
Chris Leonavicius <chris@halloapp.com>
2022-01-10 15:30:59 -0800

    Move Sentry crash reporting out of Core

--- Halloapp/AppDelegate.swift
+++ Halloapp/AppDelegate.swift
@@ -14,6 +14,7 @@ import Core
 import CoreData
 import FirebaseCrashlytics
 import Reachability
+import Sentry
 import UIKit
 import PushKit
 
@@ -32,6 +33,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         initAppContext(MainAppContext.self, serviceBuilder: serviceBuilder, contactStoreClass: ContactStoreMain.self, appTarget: AppTarget.mainApp)
+
+        #if !DEBUG
+        SentrySDK.start { options in
+            options.dsn = "https://ed03b5bdacbe4571927f8f2c93a45790@o473086.ingest.sentry.io/6126729"
+            options.maxBreadcrumbs = 500
+        }
+        SentrySDK.setUser(Sentry.User(userId: AppContext.shared.userData.userId))
+        DDLog.add(SentryLogger(logFormatter: LogFormatter()))
+        #endif
 
         DDLogInfo("application/didFinishLaunching [\(AppContext.userAgent)] [\(UIDevice.current.getModelName()) (iOS \(UIDevice.current.systemVersion))]")
 
