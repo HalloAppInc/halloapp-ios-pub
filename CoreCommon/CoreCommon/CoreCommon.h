ca46bdb1e1691c3f7fa28186436397c89d08fed1
Nandini Shetty <nandini@halloapp.com>
2022-02-22 10:26:15 -0800

    add app clip + CoreCommon + Packages (#2065)

---
+++ CoreCommon/CoreCommon/CoreCommon.h
@@ -0,0 +1,18 @@
+//
+//  CoreCommon.h
+//  CoreCommon
+//
+//  Created by Nandini Shetty on 2/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreCommon.
+FOUNDATION_EXPORT double CoreCommonVersionNumber;
+
+//! Project version string for CoreCommon.
+FOUNDATION_EXPORT const unsigned char CoreCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCommon/PublicHeader.h>
+
+
