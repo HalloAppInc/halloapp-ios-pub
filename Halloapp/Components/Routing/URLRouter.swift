0cad05b7d44234331573bc61de5cbf7311473307
Chris Leonavicius <chris@halloapp.com>
2022-03-25 14:59:56 -0700

    URL Router

---
+++ Halloapp/Components/Routing/URLRouter.swift
@@ -0,0 +1,166 @@
+//
+//  URLRouter.swift
+//  HalloApp
+//
+//  Created by Chris Leonavicius on 3/24/22.
+//  Copyright © 2022 HalloApp, Inc. All rights reserved.
+//
+
+import CocoaLumberjackSwift
+import Foundation
+
+class URLRouter {
+
+    static let shared: URLRouter = {
+        var routes: [Route] = []
+        routes.append(Route(path: "/invite") { params in
+            guard let inviteToken = params["g"] else {
+                return
+            }
+            MainAppContext.shared.userData.groupInviteToken = inviteToken
+            MainAppContext.shared.didGetGroupInviteToken.send()
+        })
+        routes.append(Route(path: "/appclip") { params in
+            guard let inviteToken = params["g"] else {
+                return
+            }
+            MainAppContext.shared.userData.groupInviteToken = inviteToken
+            MainAppContext.shared.didGetGroupInviteToken.send()
+        })
+
+        var shareRoutes: [Route] = []
+        shareRoutes.append(Route(path: "/:blobID") { params in
+            guard let blobID = params["blobID"] else {
+                return
+            }
+
+            // TODO
+
+        })
+
+        return URLRouter(hosts: [
+            Host(domains: ["share.halloapp.com"], routes: shareRoutes),
+            Host(domains: [URLRouter.applinkHost, "halloapp.com", "www.halloapp.com"], routes: routes)
+        ])
+    }()
+
+    typealias Params = [String: String]
+
+    struct Host {
+        let domains: Set<String>
+        let routes: [Route]
+
+        init(domains: [String], routes: [Route]) {
+            self.domains = Set(domains)
+            self.routes = routes
+        }
+    }
+
+    static let applinkHost = "applink"
+    static let fragmentParameter = "urlFragment"
+
+    struct Route {
+        enum PathComponent {
+            case path(path: String), parameter(name: String)
+        }
+
+        let pathComponents: [PathComponent]
+        let handler: ([String: String]) -> Void
+
+        init(path: String, handler: @escaping (Params) -> Void) {
+            self.pathComponents = path.split(separator: "/").map { component in
+                if component.hasPrefix(":") {
+                    return .parameter(name: String(component.dropFirst()))
+                } else {
+                    return .path(path: String(component))
+                }
+            }
+            self.handler = handler
+        }
+    }
+
+    private var hosts: [Host]
+
+    init(hosts: [Host]) {
+        self.hosts = hosts
+    }
+
+    @discardableResult
+    func handle(url: URL) -> Bool {
+        guard let (route, params) = route(for: url) else {
+            DDLogInfo("URLRouter/No matching route found for \(url)")
+            return false
+        }
+        DDLogInfo("URLRouter/Found matching route and will handle \(url)")
+        route.handler(params)
+        return true
+    }
+
+    func handleOrOpen(url: URL) {
+        if !handle(url: url) {
+            UIApplication.shared.open(url)
+        }
+    }
+}
+
+extension URLRouter {
+
+    private func route(for url: URL) -> (Route, Params)? {
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+
+        guard let scheme = urlComponents.scheme?.lowercased(),
+              var host = urlComponents.host?.lowercased() else {
+            return nil
+        }
+
+        var urlPathComponents = Array(urlComponents.path.split(separator: "/").map { String($0) })
+
+        switch scheme {
+        case "http", "https":
+            break
+        case "halloapp":
+            // For halloapp:// urls, the host will be the first part of the path
+            urlPathComponents.insert(host, at: 0)
+            host = Self.applinkHost
+        default:
+            return nil
+        }
+
+        guard let routes = hosts.first(where: { $0.domains.contains(host) })?.routes else {
+            return nil
+        }
+
+        var params: Params = [:]
+        let route = routes.first { route in
+            guard route.pathComponents.count == urlPathComponents.count else {
+                return false
+            }
+            params = [:]
+            return zip(route.pathComponents, urlPathComponents).allSatisfy { (routePathComponent, urlPathComponent) in
+                switch routePathComponent {
+                case .parameter(let name):
+                    params[name] = urlPathComponent
+                    return true
+                case .path(let path):
+                    return path == urlPathComponent
+                }
+            }
+        }
+
+        guard let route = route else {
+            return nil
+        }
+
+        // Route parameters trump query parameters
+        if let queryParams = urlComponents.queryItems?.reduce(into: [:], { $0[$1.name] = $1.value }) {
+            params.merge(queryParams) { (current, _) in current }
+        }
+
+        // Add in fragment
+        params[Self.fragmentParameter] = urlComponents.fragment
+
+        return (route, params)
+    }
+}
---
+++ HalloappTests/URLRouterTests.swift
@@ -0,0 +1,118 @@
+//
+//  URLRouterTests.swift
+//  HalloApp
+//
+//  Created by Chris Leonavicius on 3/25/22.
+//  Copyright © 2022 HalloApp, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import HalloApp
+
+class URLRouterTests: XCTestCase {
+
+    func testAppLinks() {
+        let expectation = expectation(description: "Should handle route")
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: [URLRouter.applinkHost], routes: [
+                URLRouter.Route(path: "/foo/bar", handler: { _ in
+                    expectation.fulfill()
+                })
+            ])
+        ])
+        router.handle(url: URL(string: "halloapp://foo/bar")!)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testUniversalLinks() {
+        let expectation = expectation(description: "Should handle route")
+        expectation.expectedFulfillmentCount = 4
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: ["www.halloapp.com", "halloapp.com"], routes: [
+                URLRouter.Route(path: "/foo/bar", handler: { _ in
+                    expectation.fulfill()
+                })
+            ])
+        ])
+        for scheme in ["http", "https"] {
+            for domain in ["www.halloapp.com", "halloapp.com"] {
+                router.handle(url: URL(string: "\(scheme)://\(domain)/foo/bar")!)
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+
+    func testRouteParameters() {
+        let expectation = expectation(description: "Should handle route")
+        expectation.expectedFulfillmentCount = 2
+
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: [URLRouter.applinkHost, "halloapp.com"], routes: [
+                URLRouter.Route(path: "/:foo/bar/:baz", handler: { params in
+                    XCTAssertEqual(params["foo"], "value1")
+                    XCTAssertEqual(params["baz"], "value2")
+                    XCTAssertEqual(params.count, 2)
+                    expectation.fulfill()
+                })
+            ])
+        ])
+
+        router.handle(url: URL(string: "halloapp://value1/bar/value2")!)
+        router.handle(url: URL(string: "https://halloapp.com/value1/bar/value2")!)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testUrlParameters() {
+        let expectation = expectation(description: "Should handle route")
+        expectation.expectedFulfillmentCount = 2
+
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: [URLRouter.applinkHost, "halloapp.com"], routes: [
+                URLRouter.Route(path: "/foo", handler: { params in
+                    XCTAssertEqual(params["bar"], "value1")
+                    XCTAssertEqual(params["baz"], "value2")
+                    XCTAssertEqual(params.count, 2)
+                    expectation.fulfill()
+                })
+            ])
+        ])
+
+        router.handle(url: URL(string: "halloapp://foo?bar=value1&baz=value2")!)
+        router.handle(url: URL(string: "https://halloapp.com/foo?bar=value1&baz=value2")!)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testFragment() {
+        let expectation = expectation(description: "Should handle route")
+
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: ["halloapp.com"], routes: [
+                URLRouter.Route(path: "/test", handler: { params in
+                    XCTAssertEqual(params[URLRouter.fragmentParameter], "fragment")
+                    XCTAssertEqual(params.count, 1)
+                    expectation.fulfill()
+                })
+            ])
+        ])
+
+        router.handle(url: URL(string: "https://halloapp.com/test#fragment")!)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testParameterOverrides() {
+        let expectation = expectation(description: "Should handle route")
+
+        let router = URLRouter(hosts: [
+            URLRouter.Host(domains: ["halloapp.com"], routes: [
+                URLRouter.Route(path: "/test/:bar", handler: { params in
+                    XCTAssertEqual(params["bar"], "url")
+                    XCTAssertEqual(params.count, 1)
+                    expectation.fulfill()
+                })
+            ])
+        ])
+
+        router.handle(url: URL(string: "https://halloapp.com/test/url?bar=query")!)
+        waitForExpectations(timeout: 1)
+    }
+}
