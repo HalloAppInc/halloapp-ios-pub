8a9322786952ac241c10617451a1c68dcab40508
Murali Balusu <murali@halloapp.com>
2022-05-06 03:42:21 +0530

    Move chat structs to core (#2536)
    - moving XMPPChatMessage structs to Core.
    - this can then be used by both the extensions and the main app.

---
+++ Core/Core/Source/Chat/XMPPChatMessage.swift
@@ -0,0 +1,102 @@
+//
+//  XMPPChatMessage.swift
+//  HalloApp
+//
+//  Created by Alan Luo on 8/3/20.
+//  Copyright © 2020 Halloapp, Inc. All rights reserved.
+//
+
+import CoreCommon
+import UIKit
+
+public struct XMPPChatMessage {
+    public let id: String
+    public let fromUserId: UserID
+    public let toUserId: UserID
+    public var retryCount: Int32? = nil
+    public var rerequestCount: Int32
+    public let content: ChatContent
+    public let context: ChatContext
+    public var timestamp: TimeInterval?
+}
+
+extension XMPPChatMessage: ChatMessageProtocol {
+
+    public var orderedMedia: [ChatMediaProtocol] {
+        switch content {
+        case .album(_, let media):
+            return media
+        case .voiceNote(let media):
+            return [media]
+        case .text, .unsupported:
+            return []
+        }
+    }
+
+    public var timeIntervalSince1970: TimeInterval? {
+        timestamp
+    }
+
+    public var linkPreviewData: [LinkPreviewProtocol] {
+        switch content {
+        case .album, .voiceNote, .unsupported:
+            return []
+        case .text(_, let linkPreviewData):
+            return linkPreviewData
+        }
+    }
+}
+
+extension XMPPChatMedia {
+    public init(chatMedia: CommonMedia) {
+        self.init(
+            url: chatMedia.url,
+            type: chatMedia.type,
+            size: chatMedia.size,
+            key: chatMedia.key,
+            sha256: chatMedia.sha256,
+            blobVersion: chatMedia.blobVersion,
+            chunkSize: chatMedia.chunkSize,
+            blobSize: chatMedia.blobSize)
+    }
+}
+
+extension XMPPChatMessage {
+
+    // for outbound message
+    public init(chatMessage: ChatMessage) {
+        self.id = chatMessage.id
+        self.fromUserId = chatMessage.fromUserId
+        self.toUserId = chatMessage.toUserId
+        self.context = ChatContext(
+            feedPostID: chatMessage.feedPostId,
+            feedPostMediaIndex: chatMessage.feedPostMediaIndex,
+            chatReplyMessageID: chatMessage.chatReplyMessageID,
+            chatReplyMessageMediaIndex: chatMessage.chatReplyMessageMediaIndex,
+            chatReplyMessageSenderID: chatMessage.chatReplyMessageSenderID)
+        self.rerequestCount = Int32(chatMessage.resendAttempts)
+
+        if let media = chatMessage.media, !media.isEmpty {
+            if media.count == 1, let item = media.first, item.type == .audio {
+                self.content = .voiceNote(XMPPChatMedia(chatMedia: item))
+            } else {
+                self.content = .album(
+                    chatMessage.rawText,
+                    media.sorted(by: { $0.order < $1.order }).map{ XMPPChatMedia(chatMedia: $0) })
+            }
+        } else {
+            self.content = .text(chatMessage.rawText ?? "", chatMessage.linkPreviewData)
+        }
+    }
+
+    public init(content: ChatContent, context: ChatContext, timestamp: Int64, from fromUserID: UserID, to toUserID: UserID, id: String, retryCount: Int32, rerequestCount: Int32) {
+        self.id = id
+        self.fromUserId = fromUserID
+        self.toUserId = toUserID
+        self.timestamp = TimeInterval(timestamp)
+        self.retryCount = retryCount
+        self.rerequestCount = rerequestCount
+        self.content = content
+        self.context = context
+    }
+}
