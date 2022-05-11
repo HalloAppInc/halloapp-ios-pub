16d46daeceec573abd81d9d23d55f5ab3522c2b5
Nandini Shetty <nandini@halloapp.com>
2022-05-11 11:56:25 -0700

    Clean Up MessageViewCell  (#2573)
    Clean Up MessageViewCell

--- Halloapp/Views/Home/Feed/Comment/FlatComment/MessageViewCell.swift
+++ Halloapp/Views/Home/Feed/Comment/FlatComment/MessageViewCell.swift
@@ -27,105 +27,11 @@ class MessageViewCell: MessageCellViewBase {
     var MinWidthOfQuotedMessageBubble: CGFloat { return contentView.bounds.width * 0.4 }
     var MediaViewDimention: CGFloat { return 238.0 }
 
-    lazy var mediaWidthConstraint = mediaCarouselView.widthAnchor.constraint(equalToConstant: MediaViewDimention)
-    lazy var mediaHeightConstraint = mediaCarouselView.heightAnchor.constraint(equalToConstant: MediaViewDimention)
-    lazy var quotedMediaMessageMinWidthConstraint = quotedMessageView.widthAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(MinWidthOfQuotedMediaMessageBubble).rounded())
-    lazy var quotedMessageMinWidthConstraint = quotedMessageView.widthAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(MinWidthOfQuotedMessageBubble).rounded())
-
-    var hasMedia: Bool = false  {
-        didSet {
-            mediaCarouselView.isHidden = !hasMedia
-        }
-    }
-
     var hasText: Bool = false  {
         didSet {
             textLabel.isHidden = !hasText
         }
     }
-
-    var hasAudio: Bool = false {
-        didSet {
-            audioView.isHidden = !hasAudio
-            audioTimeLabel.isHidden = !hasAudio
-        }
-    }
-
-    var hasLinkPreview: Bool = false {
-        didSet {
-            linkPreviewView.isHidden = !hasLinkPreview
-        }
-    }
-
-    var hasQuotedComment: Bool = false {
-        didSet {
-            quotedMessageView.isHidden = !hasQuotedComment
-        }
-    }
-
-    // MARK: Media
-
-    private lazy var mediaCarouselView: MediaCarouselView = {
-        var configuration = MediaCarouselViewConfiguration.default
-        configuration.alwaysScaleToFitContent = false
-        let mediaCarouselView = MediaCarouselView(media: [], configuration: configuration)
-        mediaCarouselView.isHidden = true
-        mediaCarouselView.delegate = self
-        return mediaCarouselView
-    }()
-
-    // MARK: Audio Media
-
-    private lazy var audioView: AudioView = {
-        let audioView = AudioView()
-        audioView.isHidden = true
-        audioView.translatesAutoresizingMaskIntoConstraints = false
-        audioView.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        audioView.delegate = self
-        return audioView
-    }()
-
-    // MARK: Link Preview
-
-    private lazy var linkPreviewView: CommentLinkPreviewView = {
-        let linkPreviewView = CommentLinkPreviewView(frame: .zero)
-        linkPreviewView.translatesAutoresizingMaskIntoConstraints = false
-        linkPreviewView.isHidden = true
-        return linkPreviewView
-    }()
-
-    private lazy var audioTimeLabel: UILabel = {
-        let label = UILabel()
-        label.isHidden = true
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 1
-        label.font = UIFont.preferredFont(forTextStyle: .caption2)
-        label.textColor = UIColor.chatTime
-
-        return label
-    }()
-    
-    private lazy var audioTimeRow: UIStackView = {
-        let leadingSpacer = UIView()
-        leadingSpacer.translatesAutoresizingMaskIntoConstraints = false
-        leadingSpacer.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        let spacer = UIView()
-        spacer.translatesAutoresizingMaskIntoConstraints = false
-        let view = UIStackView(arrangedSubviews: [ leadingSpacer, audioTimeLabel, spacer, timeLabel ])
-        view.axis = .horizontal
-        view.spacing = 0
-        view.isLayoutMarginsRelativeArrangement = true
-        return view
-    }()
-
-    // MARK: Quoted Message
-    
-    private lazy var quotedMessageView: QuotedMessageCellView = {
-        let quotedMessageView = QuotedMessageCellView()
-        quotedMessageView.translatesAutoresizingMaskIntoConstraints = false
-        quotedMessageView.isHidden = true
-        return quotedMessageView
-    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -141,12 +47,7 @@ class MessageViewCell: MessageCellViewBase {
         backgroundColor = UIColor.feedBackground
         contentView.preservesSuperviewLayoutMargins = false
         nameContentTimeRow.addArrangedSubview(nameRow)
-        nameContentTimeRow.addArrangedSubview(quotedMessageView)
-        nameContentTimeRow.addArrangedSubview(linkPreviewView)
-        nameContentTimeRow.addArrangedSubview(audioView)
-        nameContentTimeRow.addArrangedSubview(mediaCarouselView)
         nameContentTimeRow.addArrangedSubview(textRow)
-        nameContentTimeRow.addArrangedSubview(audioTimeRow)
         contentView.addSubview(messageRow)
         messageRow.constrain([.top], to: contentView)
         messageRow.constrain(anchor: .bottom, to: contentView, priority: UILayoutPriority(rawValue: 999))
@@ -158,10 +59,6 @@ class MessageViewCell: MessageCellViewBase {
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureCellAction))
         panGestureRecognizer.delegate = self
         self.addGestureRecognizer(panGestureRecognizer)
-        // Tapping on quoted comment should take you to the original comment
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(jumpToQuotedMsg(_:)))
-        quotedMessageView.isUserInteractionEnabled = true
-        quotedMessageView.addGestureRecognizer(tapGesture)
         
     }
     
@@ -171,8 +68,6 @@ class MessageViewCell: MessageCellViewBase {
             nameContentTimeRow.widthAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(MinWidthOfMessageBubble).rounded()),
             rightAlignedConstraint,
             leftAlignedConstraint,
-            mediaWidthConstraint,
-            mediaHeightConstraint
         ])
     }
 
@@ -195,21 +90,11 @@ class MessageViewCell: MessageCellViewBase {
         } else if comment.status == .unsupported {
             configureCell()
             configureUnsupportedComment(comment: comment)
-        } else {
-            configureQuotedComment(comment: comment, parentUserColorAssignment: parentUserColorAssignment)
-            configureText(comment: comment)
-            configureMedia(comment: comment)
-            configureLinkPreviewView(comment: comment)
-            configureCell()
         }
     }
 
     private func configureRetractedComment() {
         hasText = true
-        hasMedia = false
-        hasAudio = false
-        hasQuotedComment = false
-        hasLinkPreview = false
         textLabel.text = Localizations.commentDeleted
         textLabel.textColor = UIColor.chatTime
     }
@@ -218,10 +103,6 @@ class MessageViewCell: MessageCellViewBase {
         let waitingString = "🕓 " + Localizations.feedCommentWaiting
         let attributedString = Localizations.appendLearnMoreLabel(to: waitingString)
         hasText = true
-        hasMedia = false
-        hasAudio = false
-        hasQuotedComment = false
-        hasLinkPreview = false
         textLabel.attributedText = attributedString.with(font: UIFont.preferredFont(forTextStyle: .subheadline).withItalicsIfAvailable, color: .secondaryLabel)
         textLabel.textColor = UIColor.chatTime
     }
@@ -230,10 +111,6 @@ class MessageViewCell: MessageCellViewBase {
         let cryptoResultString: String = FeedItemContentView.obtainCryptoResultString(for: comment.id)
         let attributedString = NSMutableAttributedString(string: "⚠️ " + Localizations.commentIsNotSupported + cryptoResultString)
         hasText = true
-        hasMedia = false
-        hasAudio = false
-        hasQuotedComment = false
-        hasLinkPreview = false
         textLabel.attributedText = attributedString.with(font: UIFont.preferredFont(forTextStyle: .subheadline).withItalicsIfAvailable, color: .secondaryLabel)
         textLabel.textColor = UIColor.chatTime
     }
@@ -241,110 +118,10 @@ class MessageViewCell: MessageCellViewBase {
     // Adjusting constraint priorities here in a single place to be able to easily see relative priorities.
     override func configureCell() {
         super.configureCell()
-        updateMediaConstraints()
-        if isOwnMessage {
-            quotedMessageView.bubbleView.backgroundColor = UIColor.quotedMessageOwnBackground
-        } else {
-            quotedMessageView.bubbleView.backgroundColor = UIColor.quotedMessageNotOwnBackground
-        }
-    }
-
-    private func updateMediaConstraints() {
-        mediaWidthConstraint.isActive = false
-        mediaHeightConstraint.isActive = false
-        quotedMediaMessageMinWidthConstraint.isActive = false
-        quotedMessageMinWidthConstraint.isActive = false
-        if hasMedia || hasAudio {
-            mediaWidthConstraint.isActive = true
-        } else if (hasQuotedComment && quotedMessageView.hasMedia) ||  hasLinkPreview {
-            // If quoted comments have media, set the min width of the comment.
-            quotedMediaMessageMinWidthConstraint .isActive = true
-        } else if (hasQuotedComment && !quotedMessageView.hasMedia) {
-            quotedMessageMinWidthConstraint .isActive = true
-        }
-        if hasMedia {
-            mediaHeightConstraint.isActive = true
-        }
     }
 
     private func setNameLabel(for userID: String) {
         nameLabel.text = MainAppContext.shared.contactStore.fullName(for: userID)
-    }
-
-    private func configureQuotedComment(comment: FeedPostComment, parentUserColorAssignment: UIColor) {
-        if let parentComment = comment.parent {
-            quotedMessageView.configureWithComment(comment: parentComment, userColorAssignment: parentUserColorAssignment)
-            hasQuotedComment = true
-        } else {
-            hasQuotedComment = false
-        }
-    }
-
-    private func configureMedia(comment: FeedPostComment) {
-        guard let commentMedia = comment.media, commentMedia.count > 0 else {
-            hasMedia = false
-            hasAudio = false
-            return
-        }
-        guard let media = MainAppContext.shared.feedData.media(commentID: comment.id) else {
-            hasMedia = false
-            hasAudio = false
-            return
-        }
-        // Download any pending media, comes in handy for media coming in while user is viewing comments
-        MainAppContext.shared.feedData.downloadMedia(in: [comment])
-        MainAppContext.shared.feedData.loadImages(commentID: comment.id)
-        // Check for voice note
-        if commentHasAudio(media: media) {
-            configureAudio(comment: comment, audioMedia: media[0])
-            return
-        }
-        hasMedia = true
-        mediaCarouselView.configureMediaCarousel(media: media)
-    }
-
-    private func commentHasAudio(media: [FeedMedia]) -> Bool {
-        if media.count == 1 && media[0].type == .audio {
-            hasAudio = true
-        } else {
-            hasAudio = false
-        }
-        return hasAudio
-    }
-
-    func configureAudio(comment: FeedPostComment, audioMedia: FeedMedia) {
-        if audioView.url != audioMedia.fileURL {
-            audioTimeLabel.text = "0:00"
-            audioView.url = audioMedia.fileURL
-        }
-
-        if !audioView.isPlaying {
-            let isOwn = comment.userId == MainAppContext.shared.userData.userId
-            audioView.state = comment.status == .played || isOwn ? .played : .normal
-        }
-
-        if audioMedia.fileURL == nil {
-            audioView.state = .loading
-
-            audioMediaStatusCancellable = audioMedia.mediaStatusDidChange.sink { [weak self] mediaItem in
-                guard let self = self else { return }
-                guard let url = mediaItem.fileURL else { return }
-                self.audioView.url = url
-
-                let isOwn = comment.userId == MainAppContext.shared.userData.userId
-                self.audioView.state = comment.status == .played || isOwn ? .played : .normal
-            }
-        }
-    }
-
-    func configureLinkPreviewView(comment: FeedPostComment) {
-        guard let feedLinkPreviews = comment.linkPreviews, let feedLinkPreview = feedLinkPreviews.first else {
-            hasLinkPreview = false
-            return
-        }
-        hasLinkPreview = true
-        MainAppContext.shared.feedData.loadImages(feedLinkPreviewID: feedLinkPreview.id)
-        linkPreviewView.configure(linkPreview: feedLinkPreview)
     }
 }
 
@@ -363,19 +140,5 @@ extension MessageViewCell: MediaCarouselViewDelegate {
     }
 
     func mediaCarouselView(_ view: MediaCarouselView, didZoomMediaAtIndex index: Int, withScale scale: CGFloat) {
-    }
-}
-
-// MARK: AudioViewDelegate
-extension MessageViewCell: AudioViewDelegate {
-    func audioView(_ view: AudioView, at time: String) {
-        audioTimeLabel.text = time
-    }
-    func audioViewDidStartPlaying(_ view: AudioView) {
-        guard let commentId = feedPostComment?.id else { return }
-        audioView.state = .played
-        MainAppContext.shared.feedData.markCommentAsPlayed(commentId: commentId)
-    }
-    func audioViewDidEndPlaying(_ view: AudioView, completed: Bool) {
     }
 }
