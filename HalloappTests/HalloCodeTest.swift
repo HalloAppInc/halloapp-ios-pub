0e7ce4e07c6ab43bca26caa4573fd35c0cb30601
Chris Leonavicius <chris@halloapp.com>
2023-05-05 12:41:17 -0700

    Update CI to run on MacOS 13 / Xcode 14.3
    # Conflicts:
    #	.github/workflows/MasterWorkflow.yml

--- HalloappTests/HalloCodeTest.swift
+++ HalloappTests/HalloCodeTest.swift
@@ -21,41 +21,33 @@ extension HalloCodeTest {
 class HalloCodeTest: XCTestCase {
     /// Tests that our codes scan correctly.
     func testDecoding() throws {
-        let codes = try createCodes()
-        for code in codes {
-            let decoded = decode(code)
-            XCTAssertEqual(decoded, code.string)
-        }
-    }
-    
-    private func createCodes() throws -> [HalloCode] {
-        var codes = [HalloCode]()
         for url in HalloCodeTest.strings {
             for size in HalloCodeTest.sizes {
-                // create the same code at different sizes
-                guard let code = HalloCode(size: size, string: url) else {
-                    throw HalloCodeError.couldNotCreate(url)
+                try autoreleasepool {
+                    // create the same code at different sizes
+                    guard let code = HalloCode(size: size, string: url) else {
+                        throw HalloCodeError.couldNotCreate(url)
+                    }
+
+                    let decoded = decode(code)
+                    XCTAssertEqual(decoded, code.string)
                 }
-                
-                codes.append(code)
             }
         }
-        
-        return codes
     }
 
     private func decode(_ code: HalloCode) -> String? {
         let image = code.image
         guard
-            let detector = CIDetector(ofType: CIDetectorTypeQRCode,context: nil,
-                                     options: [CIDetectorAccuracy: CIDetectorAccuracyHigh]),
+            let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil,
+                                      options: [CIDetectorAccuracy: CIDetectorAccuracyHigh]),
             let ci = CIImage(image: image),
             let features = detector.features(in: ci) as? [CIQRCodeFeature],
             features.count > 0
         else {
             return nil
         }
-        
+
         return features.first?.messageString
     }
     
@@ -96,8 +88,6 @@ extension HalloCodeTest {
         "https://www.nybooks.com/articles/2022/01/13/apotheosis-now/",
         "https://kottke.org",
         "https://www.theverge.com/2022/1/20/22893059/messenger-new-kids-internet-safety-games-facebook-meta",
-        "07037 65770 49341 33913 42533 54878 11403 98260 05887 37578 51633 28694",
-        "Specifically, Apple’s security concerns relate to language in the bill that the company fears would allow iPhone and iPad users to download apps outside of the App Store. Powderly argued this provision could harm users who could download unscreened and potentially harmful software to their devices. If enacted, the bill would make it difficult for Apple to collect its sometimes 30 percent commissions from developers on App Store purchases."
     ]
     
     private static let sizes = [
